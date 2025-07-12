79b9b4affb3dab6444b047fcc32bc847e5f7c0e2
Yuvraj Singh <124923698+yuvrajsingh8048@users.noreply.github.com>
2025-07-12 11:37:09 +0530

    subhero section update

--- src/components/heroSection/SubHeroSection.jsx
+++ src/components/heroSection/SubHeroSection.jsx
@@ -9,9 +9,9 @@ const SubHeroSection = () => {
       initial="hidden"
       whileInView="show"
       viewport={{once: false, amount: 0}}
-    className='bg-lightBrown flex my-10 justify-around px-4 border-y border-lightGrey uppercase xl:text-4xl md:text-2xl sm:text-4xl items-center p-3'> 
-      <p className='md:block sm:hidden'> Fast Learner</p>
-      <p className='md:block sm:hidden'>Team Work</p>
+    className='bg-lightBrown flex my-10 justify-around px-4 border-y border-lightGrey uppercase xl:text-4xl lg:text-2xl md:text-lg sm:text-sm sm:font-semibold items-center p-3'> 
+      <p>Fast Learner</p>
+      <p>Team Work</p>
       <p>Problem Solving</p>
     </motion.div>
   )
